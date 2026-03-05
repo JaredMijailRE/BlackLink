4ecb466fa2a9298d23e560fcc2a4d9ad35427a16
jared <jramirezes@unal.edu.co>
2026-03-04 19:08:46 -0500

    readme

--- app/src/main/java/com/example/blacklink/MainActivity.kt
+++ app/src/main/java/com/example/blacklink/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             BlackLinkTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "vane",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
